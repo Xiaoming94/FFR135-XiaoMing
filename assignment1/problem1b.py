40b3ffcbe265c6de655467ddf0d3cc0f8b670727
Henry Yang <mail@xiaoming.se>
2018-09-16 12:41:20 +0200

    Comment hell

--- assignment1/problem1b.py
+++ assignment1/problem1b.py
@@ -19,13 +19,28 @@ for p in patterns :
     errors = 0
     for i in range(trials):
         net = HopField(p,bitts)
-        
+          
         # Randomly chooose a pattern and which neuron to observe
         n_i = np.random.randint(bitts)
         p_i = np.random.randint(p)
         inputPattern = net.patterns[p_i]
         
-        # Feed the pattern and check the results
+
+        # Set Diagonal to Zeroes
+        #for i in range(bitts):
+        #    net.weights[i,i] = 0 
+        # Set Diagonal to Zeroes
+        #for i in range(bitts):
+        #    net.weights[i,i] = 0
+        # Set Diagonal to Zeroes
+        #for i in range(bitts):
+        #    net.weights[i,i] = 0
+        # Set Diagonal to Zeroes
+        #for i in range(bitts):
+        #    net.weights[i,i] = 0
+        # Set Diagonal to Zeroes
+        #for i in range(bitts):
+        #    net.weights[i,i] = 0        # Feed the pattern and check the results
         # Feeding the pattern using asynchronous update
         neuronState = net.feedAsync(inputPattern[:,0],n_i)
         if(neuronState != inputPattern[n_i,0]):
