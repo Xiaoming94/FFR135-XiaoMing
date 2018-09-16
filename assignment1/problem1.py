67300e620065ef18c0750abb0a40f86e6332cafb
Henry Yang <mail@xiaoming.se>
2018-09-16 12:39:16 +0200

    Done with problem 1 a

--- assignment1/problem1.py
+++ assignment1/problem1.py
@@ -1,18 +1,40 @@
+# problem1.py
+# Author: Henry Yang(940503-1056)
+
 import numpy as np
 from hopfieldnetwork import HopField
 
-patterns = [12,20,40,60,80,100]
-trials = 10 ** 5
-bitts = 100
+#########################################################
+# This is the source file for solving the first part of #
+# problem 1 in Assignment1 of FFR135                    #
+# The parameters chosen are given by the Assignnment    #
+#########################################################
+
+##
+# Setting Parameters
+patterns = [12,20,40,60,80,100] # Pattern counts to test
+trials = 10 ** 5    # Number of Independent trials
+bitts = 100         # Number of bitts in each pattern
 p_errors_list = []
+
+# Performing the simulation
 
 for p in patterns :
     errors = 0
     for i in range(trials):
         net = HopField(p,bitts)
+
+        # Set Diagonal to Zeroes
+        for i in range(bitts):
+            net.weights[i,i] = 0
+
+        # Randomly chooose a pattern and which neuron to observe
         n_i = np.random.randint(bitts)
         p_i = np.random.randint(p)
         inputPattern = net.patterns[p_i]
+        
+        # Feed the pattern and check the results
+        # Feeding the pattern using asynchronous update
         neuronState = net.feedAsync(inputPattern[:,0],n_i)
         if(neuronState != inputPattern[n_i,0]):
             errors += 1
@@ -22,5 +44,6 @@ for p in patterns :
 
     p_errors_list.append(p_error)
 
+print(p_errors_list)
 
         
