0799a7289757d0b9912b6346b99c9e36bf1fe62e
Henry Yang <mail@xiaoming.se>
2018-10-17 01:33:13 +0200

    Add successful Perceptron implementation

--- assignment3/perceptron.py
+++ assignment3/perceptron.py
@@ -9,7 +9,12 @@ import numpy as np
 
 class PerceptronLayer:
     def __init__(self, input, output, activation = np.tanh):
-        self.weights = np.random.normal(0,1,size=(output,input))
+        self.neurons = output
+        weights = np.random.normal(0,1,size=(output,input))
+        if output == 1:
+            weights = weights.reshape(output,input)
+        
+        self.weights = weights
         self.thresholds = np.zeros(output)
         self.activation = activation
     
@@ -19,6 +24,7 @@ class PerceptronLayer:
         synapse_in = self.weights @ input_pattern
         synapse_in = np.transpose(synapse_in) - self.thresholds
         return np.transpose(self.activation(synapse_in))
+
 ##
 # The Config object is a dictionary that contains the keys input, output, and hidden
 # Input is an integer
@@ -31,13 +37,52 @@ class Perceptron:
     
     def feed_forward(self, input_pattern):
         states = []
-        current_state = input_pattern
+        current_state = np.transpose(input_pattern)
         for l in self.layers:
+           
+            states.append(np.transpose(current_state))
             current_state = l.feed(current_state)
-            states.append(current_state)
         
-        return current_state,states
+        return np.transpose(current_state), states
 
+    def train(self, epochs, batchsize, 
+        learning_rate, train_data, train_targets,
+        val_data, val_targets):
+        data_count = train_data.shape[0]
+        indices = np.arange(data_count)
+        val_energy_vec = np.zeros(epochs)
+        for i in range(epochs):
+            batches = np.reshape(np.random.permutation(indices),(-1,batchsize))
+            for batch in batches:
+                self.backprop_update(learning_rate, train_data[batch], train_targets[batch],batchsize)
+            
+            output, _ = self.feed_forward(val_data)
+            val_energy = energy_function(output,val_targets)
+            print("current validation energy: %s" % val_energy)
+            val_energy_vec[i] = val_energy
+
+        return val_energy_vec
+    
+    def backprop_update(self, learning_rate, data, targets, batchsize):
+
+        output, states = self.feed_forward(data)
+        list_lsp = list(zip(self.layers,states))
+        delta = 1/batchsize * np.sum((targets - output),axis=0) * -1
+        current_states = 1/batchsize * np.sum(output,axis=0)
+        for l,s in reversed(list_lsp):
+            diffg = differentiate(l.activation,current_states)
+            delta = delta * diffg
+            pstates = np.sum(s,axis=0) * 1/batchsize
+            pstates = pstates.reshape(1,np.size(pstates))
+            dw = delta.reshape(np.size(delta),1) @ pstates
+            l.weights -= learning_rate * dw
+            l.thresholds -= learning_rate * delta.flatten() * (-1)
+            delta = np.transpose(l.weights) @ np.transpose(delta)
+            delta = np.transpose(delta)
+            current_states = pstates
+
+   
+            
 def construct_layers(config):
     layers = []
     before = config['input']
@@ -50,3 +95,12 @@ def construct_layers(config):
     output = config['output']
     layers.append(PerceptronLayer(before,output['count'],output['activation']))
     return layers
+
+def energy_function(output, targets):
+    return 1/2 * np.sum((targets - output)**2)
+
+def differentiate(activation,current_states):
+       gradients = {
+           'tanh'    : (1 - current_states ** 2)
+       }
+       return gradients[activation.__name__]
--- assignment3/problem1.py
+++ assignment3/problem1.py
@@ -3,29 +3,85 @@
 
 import numpy as np
 from perceptron import Perceptron
+import os
+from mlxtend.data import loadlocal_mnist
+import matplotlib.pyplot as plt
+
+###
+# Function that loads the MNIST data-set
+###
+def load_data(path=os.path.join("."),type="train"):
+    filenames = {
+        'train' : ('train-images-idx3-ubyte','train-labels-idx1-ubyte'),
+        'val'   : ('t10k-images-idx3-ubyte','t10-labels-idx1-ubyte')
+    }
+
+    data_file, labels_file = filenames[type]
+
+    data, labels = loadlocal_mnist(
+        images_path=os.path.join(path,data_file),
+        labels_path=os.path.join(path,labels_file)
+    )
+
+    return data,labels
+
+##
+# Verification function
+#
 
 def print_network(net):
     for l in net.layers :
+        print("\n======= NEW LAYER =======\n")
         print(l.weights)
         print(l.weights.shape)
         print(l.thresholds)
 
 output_config = {
-    'count' : 2,
+    'count' : 1,
+    'activation': np.tanh
+}
+
+hidden1_config = {
+    'count' : 10,
     'activation': np.tanh
 }
 
 config = {
-    'input' : 10,
+    'input' : 2,
     'output' : output_config,
-    'hidden' : []
+    'hidden' : [hidden1_config]
 }
 
+def gen_testdata(data_num):
+    data_pos = np.random.normal(0,1,size=(data_num,2))
+    max_x = np.max(data_pos[:,0])
+    max_y = np.max(data_pos[:,1])
+
+    data_neg = np.random.rand(data_num,2) * 2 - 1
+    data_neg = data_neg * np.array([max_x,max_y])
+
+    labelpos = np.ones(data_num)
+    labelneg = -1 * np.ones(data_num)
+
+    data = np.concatenate((data_pos,data_neg),axis=0)
+    labels = np.concatenate((labelpos,labelneg),axis=None)
+
+    return data, labels
+
+
+epochs = 40
+batchsize = 10
+
+train_data, train_labels = gen_testdata(5000)
+val_data, val_labels = gen_testdata(1000)
+
+train_labels = train_labels.reshape(np.size(train_labels),1)
+
 net = Perceptron(config)
-test_input = np.random.random(10)
-print(test_input)
+
 print_network(net)
 
-output, states = net.feed_forward(test_input)
+val_energy = net.train(epochs, batchsize,0.01,train_data,train_labels,val_data,val_labels)
 
-print(output)
+plt.plot(np.arange(epochs),val_energy)
+plt.show()
